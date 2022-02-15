abcd052d2af226c39bd95c830e9bcb5c931cccf3
hectapia <oli21007@byui.edu>
2022-02-15 01:27:05 -0600

    Header, footer, main updated

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,6 +1,31 @@
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+//const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+//datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById('currentyear').innerHTML = year;
+document.getElementById('lastupdated').textContent = `Last Updated: ${document.lastModified}`;
