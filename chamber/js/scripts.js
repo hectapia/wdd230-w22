3c810453f11476891d3c3b6d9e94227285368528
hectapia <oli21007@byui.edu>
2022-02-17 01:59:18 -0600

    Main and footer updated

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -29,3 +29,13 @@ let date = new Date();
 let year = date.getFullYear();
 document.getElementById('currentyear').innerHTML = year;
 document.getElementById('lastupdated').textContent = `Last Updated: ${document.lastModified}`;
+
+!function(d,s,id) {
+    var js,fjs=d.getElementsByTagName(s)[0];
+    if(!d.getElementById(id))
+        {js=d.createElement(s);
+            js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+}
+    (document,'script','weatherwidget-io-js');
